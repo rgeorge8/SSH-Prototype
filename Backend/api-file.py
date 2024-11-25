ea2605660047d17f34acd683863084938b1c2dd9
Wayne <wayneezechukwu27@gmail.com>
2024-11-25 22:14:52 +0000

    this is the api do what u want

---
+++ Backend/api-file.py
@@ -0,0 +1,21 @@
+import requests
+
+
+api_url = "https://api.spoonacular.com/recipes/complexSearch"
+api_key = "c0cef1272563483684104551eb97978e"  #replace api key pls
+query_params = {
+    "query": "pasta",  
+    "diet": "vegetarian",  
+
+    "apiKey": api_key
+}
+
+response = requests.get(api_url, params=query_params)
+
+
+if response.status_code == 200:#good
+    data = response.json()  
+    print("Recipes found:", data)
+else:#error
+    print(f"Failed to fetch data. Status code: {response.status_code}")
+    print("Response:", response.text)
